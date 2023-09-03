83b32767cad104c398745aecac1becd4d33bc8b8
Arun Nama <arun.nama@gmail.com>
2023-09-03 14:31:51 +0530

    Initial Commit

---
+++ Letters/LettersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LettersApp.swift
+//  Letters
+//
+//  Created by Arun Kumar Nama on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LettersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
