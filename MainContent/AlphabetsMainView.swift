2cdd8dafd60951869554349b8b2017ecce134f3b
Arun Nama <arun.nama@gmail.com>
2023-09-04 15:48:40 +0530

    Before refactor

--- MainContent/AlphabetsMainView.swift
+++ MainContent/AlphabetsMainView.swift
@@ -5,8 +5,68 @@
 //  Created by Arun Kumar Nama on 3/9/23.
 //
 
+
+
 import SwiftUI
 import Dispatch
+import AVFoundation
+import Combine
+
+class AlphabetViewModel: ObservableObject {
+    @Published var alphabets: [AlphabetItem] = []
+
+    init() {
+        fetchAlphabets()
+    }
+
+    func fetchAlphabets() {
+        if let url = URL(string: "https://raw.githubusercontent.com/arunnama/traindemo2/main/alphabets.json") {
+            URLSession.shared.dataTask(with: url) { data, _, error in
+                if let data = data {
+                    do {
+                        let decodedData = try JSONDecoder().decode(AlphabetData.self, from: data)
+                        DispatchQueue.main.async {
+                            self.alphabets = decodedData.alphabets
+                        }
+                    } catch {
+                        print("Error decoding JSON: \(error)")
+                    }
+                } else if let error = error {
+                    print("Error fetching JSON data: \(error)")
+                }
+            }.resume()
+        }
+    }
+}
+
+
+@main
+struct KidsLearningApp: App {
+    @State private var showMenu = false // Track the menu visibility
+
+    var body: some Scene {
+        WindowGroup {
+            StartupView(showMenu: $showMenu)
+        }
+    }
+}
+
+
+
+
+class TextToSpeechManager: ObservableObject {
+    private let synthesizer = AVSpeechSynthesizer()
+
+    func speak(_ text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US") // Set the language
+        synthesizer.speak(utterance)
+    }
+
+    func stopSpeaking() {
+        synthesizer.stopSpeaking(at: .immediate)
+    }
+}
 
 class TimerManager: ObservableObject {
     @Published var timer: DispatchSourceTimer?
@@ -27,7 +87,165 @@ class TimerManager: ObservableObject {
     }
 }
 
+
+struct AlphabetData: Codable {
+    let alphabets: [AlphabetItem]
+}
+
+struct AlphabetItem: Codable, Identifiable {
+    let id = UUID()
+    let letter: String
+}
+
+
+
+struct StartupView: View {
+    @Binding var showMenu: Bool
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Kids Learning App")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 20)
+
+                NavigationLink("", destination: ContentView(showMenu: $showMenu), isActive: $showMenu)
+                    .hidden() // Hidden link to navigate to the main content view
+
+                Button(action: {
+                    withAnimation {
+                        showMenu.toggle() // Show the menu
+                    }
+                }) {
+                    Text("Start Learning")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @Binding var showMenu: Bool
+    @State private var selectedDataSource: DataSource = .alphabet
+    @State private var currentPage = 0
+    @ObservedObject var ttsManager = TextToSpeechManager()
+    @State private var isAutoScrolling = false // Track auto-scrolling state
+    @StateObject var timerManager = TimerManager()
+    @ObservedObject var alphabetViewModel = AlphabetViewModel() // Initialize AlphabetViewModel
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Kids Learning App")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 20)
+
+                Picker("Select Data Source", selection: $selectedDataSource) {
+                    Text("Alphabet").tag(DataSource.alphabet)
+                    Text("Numbers").tag(DataSource.numbers)
+                    Text("Words").tag(DataSource.words)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+
+                switch selectedDataSource {
+                case .alphabet:
+                    AlphabetPageView(currentPage: $currentPage, alphabets: alphabetViewModel.alphabets, ttsManager: ttsManager) // Pass the alphabets
+                        .frame(height: UIScreen.main.bounds.height * 0.7)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(20)
+                        .padding()
+
+                case .numbers:
+                    AlphabetAppView()
+
+                case .words:
+                    AlphabetAppView()
+                }
+
+                Spacer()
+
+                HStack(spacing: 20) {
+                    Button(action: {
+                        withAnimation {
+                            showMenu.toggle() // Show the menu
+                        }
+                    }) {
+                        Text("Menu")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+
+                    Button(action: {
+                        ttsManager.speak("Your Text Here") // Replace with the text to speak
+                    }) {
+                        Image(systemName: "play.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(isAutoScrolling ? .gray : .blue)
+                            .padding()
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .disabled(isAutoScrolling)
+                    }
+
+                    Button(action: {
+                        ttsManager.stopSpeaking()
+                        timerManager.stopAutoScrolling() // Stop auto-scrolling
+                        isAutoScrolling = false
+                    }) {
+                        Image(systemName: "stop.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.red)
+                            .padding()
+                            .background(Color.white)
+                            .clipShape(Circle())
+                    }
+
+                    Button(action: {
+                        isAutoScrolling.toggle()
+                        if isAutoScrolling {
+                            timerManager.startAutoScrolling(withInterval: 1.0) {
+                                if currentPage < alphabetViewModel.alphabets.count - 1 {
+                                    currentPage += 1
+                                } else {
+                                    isAutoScrolling = false
+                                }
+                            }
+                        } else {
+                            timerManager.stopAutoScrolling() // Stop auto-scrolling when paused
+                        }
+                    }) {
+                        Image(systemName: "arrow.right.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(isAutoScrolling ? .gray : .blue)
+                            .padding()
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .disabled(isAutoScrolling)
+                    }
+                }
+            }
+            .animation(.default)
+            .navigationBarTitle("", displayMode: .inline)
+        }
+    }
+}
+
+
+struct AlphabetAppView: View {
     @ObservedObject var alphabetViewModel = AlphabetViewModel()
     @State private var currentPage = 0
     @ObservedObject var ttsManager = TextToSpeechManager()
@@ -219,14 +437,67 @@ extension Color {
     }
 }
 
-@main
-struct PageViewApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+
+class NumberItem: Identifiable {
+    let id: UUID
+    let number: Int
+
+    init(number: Int) {
+        self.id = UUID()
+        self.number = number
+    }
+}
+
+class WordItem: Identifiable {
+    let id: UUID
+    let word: String
+
+    init(word: String) {
+        self.id = UUID()
+        self.word = word
+    }
+}
+
+enum DataSource {
+    case alphabet
+    case numbers
+    case words
+
+    var title: String {
+        switch self {
+        case .alphabet:
+            return "Alphabet"
+        case .numbers:
+            return "Numbers"
+        case .words:
+            return "Words"
         }
     }
 }
+
+
+class NumberViewModel: ObservableObject {
+    @Published var numbers: [NumberItem]
+
+    init() {
+        // Dummy data for numbers (1 to 10)
+        self.numbers = (1...10).map { number in
+            NumberItem(number: number)
+        }
+    }
+}
+
+class WordViewModel: ObservableObject {
+    @Published var words: [WordItem]
+
+    init() {
+        // Dummy data for words
+        self.words = ["Apple", "Banana", "Cat", "Dog", "Elephant"].map { word in
+            WordItem(word: word)
+        }
+    }
+}
+
 
 
 
